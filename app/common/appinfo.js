93f13b7c749ccb0f76b652f647ab37e9a5e74a46
joe.pinsonault <joe.pinsonault@afc1bc61-ecbe-9bc6-ee4f-2dfcb1c07a4b>
2010-04-10 11:05:36 +0000

    Added some missing files

---
+++ app/common/appinfo.js
@@ -0,0 +1,9 @@
+AppInfo = {};
+
+AppInfo.authorEmail = "joe.pinsonault@gmail.com";
+AppInfo.authorName = "Joe Pinsonault";
+AppInfo.title = Mojo.appInfo.title;
+AppInfo.authorWebsite = "http://www.google.com/profiles/joe.pinsonault";
+AppInfo.projectWebsite = "http://code.google.com/p/webos-trimet-tracker/";
+AppInfo.version = Mojo.appInfo.version;
+AppInfo.vendor = Mojo.appInfo.vendor;
